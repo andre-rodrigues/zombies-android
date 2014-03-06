73c2e6c7c55fbcc518443fde3202a7548229e799
Andre <andre.rodrigues@tagview.com.br>
2014-03-06 16:15:25 -0300

    Tentativa de adicionar zumbis ao clicar na tela

--- src/com/example/snacktime/GameActivity.java
+++ src/com/example/snacktime/GameActivity.java
@@ -2,10 +2,13 @@ package com.example.snacktime;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnTouchListener;
 import android.widget.RelativeLayout;
 
 public class GameActivity extends Activity {
-	
+	RelativeLayout gameView;
 	private Game game;
 
 	@Override
@@ -15,22 +18,41 @@ public class GameActivity extends Activity {
 		
 		game = Game.getInstance();
 		
-		RelativeLayout baseView = (RelativeLayout) findViewById(R.id.layout);
+		gameView = (RelativeLayout) findViewById(R.id.layout);
 
-		int direction = 1;
 		for (int i = 0; i < 100; i++) {
-			direction = (i % 2) == 0 ? 1 : -1;
-			Zombie zombie = new Zombie(this, i * 5, i * 5, 1 * direction, i);
+			Zombie zombie = new Zombie(this, 
+					Math.random() * 500, 
+					Math.random() * 700, 
+					Math.cos(Math.random() * 50), 
+					Math.sin(Math.random()) * 10);
+			
 			zombie.startAnimation();
-			baseView.addView(zombie);
+			gameView.addView(zombie);
 			
 			game.addGameObject(zombie);	
 		}
 		
-		Zombie zombie = new Zombie(this, 80, 80, 0.1, 0.1);
-		zombie.startAnimation();
-		baseView.addView(zombie);
-		game.addGameObject(zombie);
+		gameView.setOnTouchListener(new OnTouchListener() {
+			
+			@Override
+			public boolean onTouch(View arg0, MotionEvent event) {
+				if (event.getPointerCount() > 1 && game.getGameObjects().size() > 0) {
+					game.removeGameObject(0);
+				} else {
+					Zombie zombie = new Zombie(GameActivity.this, 
+							Math.random() * 500, 
+							Math.random() * 700, 
+							Math.cos(Math.random() * 50), 
+							Math.sin(Math.random()) * 10);
+					
+					zombie.startAnimation();
+					gameView.addView(zombie);
+					game.addGameObject(zombie);
+				}
+				return false;
+			}
+		});
 		
 		game.start();
 	}
